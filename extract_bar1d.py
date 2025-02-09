2f1f04dee83261f21fe3714f07b5031ce86ccacf
BeGeWei <grey@pku.edu.cn>
2025-02-09 23:22:39 +0800

    optimize extract_bar1d

--- extract_bar1d.py
+++ extract_bar1d.py
@@ -1,57 +1,70 @@
 import polars as pl
 import argparse
-import os
+import glob
 
-
+# 使用wind行情序列仅仅导出"换手率"，其他字段均由aiquant导出
 # 使用wind导出的时候，不要选择复权净值
-def extract_hq(indir: str, outdir: str):
-    os.makedirs(outdir, exist_ok=True)
-    df = (
-        pl.scan_csv(
-            f"{indir}/*",
-            skip_rows=1,
-            has_header=False,
-            encoding="utf8-lossy",
-            schema={
-                "code": pl.Utf8,
-                "name": pl.Utf8,
-                "dt": pl.Date,
-                "preclose": pl.Float64,
-                "open": pl.Float64,
-                "high": pl.Float64,
-                "low": pl.Float64,
-                "close": pl.Float64,
-                "volume": pl.UInt64,
-                "amount": pl.Float64,
-                "turnover": pl.Float64,
-                "netvalue": pl.Float64,
-                "useless": pl.UInt8,  # necessary
-            },
+
+
+def extract_quote(indir: str, aiquant_file: str):
+    # read from wind csv files: code, dt, close, turnover
+    df_wind = (
+        pl.concat(
+            [
+                pl.read_csv(
+                    filename,
+                    encoding="gbk",
+                    columns=[
+                        "代码",
+                        "日期",
+                        "收盘价(元)",
+                        "换手率(%)",
+                    ],
+                    schema_overrides={
+                        "代码": pl.Utf8,
+                        "日期": pl.Date,
+                        "收盘价(元)": pl.Float64,
+                        "换手率(%)": pl.Float64,
+                    },
+                    new_columns=[
+                        "code",
+                        "dt",
+                        "close",
+                        "turnover",
+                    ],
+                )
+                for filename in glob.glob(f"{indir}/*")
+            ]
         )
-        .select(
+        .with_columns(
             pl.col("code").str.slice(0, 6).cast(pl.UInt32),
-            "dt",
-            (pl.col("preclose") * 1e4).round(0).cast(pl.UInt32),
-            (pl.col("open") * 1e4).round(0).cast(pl.UInt32),
-            (pl.col("high") * 1e4).round(0).cast(pl.UInt32),
-            (pl.col("low") * 1e4).round(0).cast(pl.UInt32),
             (pl.col("close") * 1e4).round(0).cast(pl.UInt32),
-            pl.col("volume").fill_null(0).cast(pl.UInt64),
-            (pl.col("amount") * 1e4).round(0).fill_null(0).cast(pl.UInt64),
-            "turnover",
-            (pl.col("netvalue") * 1e4).round(0).cast(pl.UInt32),
         )
         .sort(by=["code", "dt"])
-        .collect()
     )
-
+    # read from aiquant: code, dt, preclose, open, high, low, close, volume, amount, trades_count, adjfactor
+    df_aiquant = pl.read_ipc(aiquant_file)
+    # join data & turnover -> preunit
+    df = (
+        df_aiquant.join(df_wind, on=["code", "dt", "close"])
+        .with_columns(
+            (pl.col("volume") / pl.col("turnover")).alias("preunit"),
+        )
+        .with_columns(
+            pl.when(pl.col("preunit").is_infinite()).then(None).otherwise("preunit").alias("preunit"),
+        )
+        .with_columns(
+            pl.col("preunit").fill_null(strategy="forward").over("code").round(0).cast(pl.UInt64),
+        )
+        .select(pl.exclude("turnover"))
+    )
     last_dt = df.item(-1, 1)
-    df.write_ipc(f"wind-bar1d-{last_dt}.ipc", compression="zstd")
+    df.write_ipc(f"etf-bar1d-until-{last_dt}.ipc", compression="zstd")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="提取万得行情序列csv的数据")
-    parser.add_argument("-indir", type=str, required=True, help="万得导出的目录, like D:/wind_etf_bar1d")
-    parser.add_argument("-outdir", type=str, required=True, help="ipc output dir")
+    parser.add_argument("-wi", type=str, required=True, help="万得导出csv的目录, like G:/wind_etf_bar1d")
+    parser.add_argument("-ai", type=str, required=True, help="aiquant导出的文件, like aiq-etf-bar1d-until-2025-02-07.ipc")
     args = parser.parse_args()
-    extract_hq(args.indir, args.outdir)
+    extract_quote(args.wi, args.ai)
