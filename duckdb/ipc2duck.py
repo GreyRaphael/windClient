f52bf6244ea2234f715baea1990024d2879312dc
ali_gewei <grey@pku.edu.cn>
2025-02-11 09:38:53 +0800

    rm useless modules

--- duckdb/ipc2duck.py
+++
@@ -1,7 +0,0 @@
-import polars as pl
-import duckdb
-
-df = pl.read_ipc("20241126sector.ipc")
-
-with duckdb.connect("bar1d.db", read_only=False) as conn:
-    conn.execute("CREATE TABLE etf AS SELECT * FROM df")
