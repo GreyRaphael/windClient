ed7916ed06713f5416aa67bc0e6017f289051b92
BeGeWei <grey@pku.edu.cn>
2025-02-19 20:28:59 +0800

    add `wsd_download_unit`

--- task_daily_etf.py
+++ task_daily_etf.py
@@ -19,6 +19,18 @@ def wsd_download_daily(codes: list[str], target_dt: dt.date):
     return pl.concat(dfs)
 
 
+def wsd_download_unit(codes: list[str], target_dt: dt.date):
+    # codes like [510050.OF,]
+    dfs = []
+    for code in codes:
+        record = w.wsd(code, "unit_floortrading", target_dt, target_dt, "unit=1")
+        df = pl.from_records([record.Codes] + [record.Times] + record.Data, schema={"code": pl.Utf8, "dt": pl.Date, "unit": pl.Float64}).with_columns(
+            pl.col("code").str.slice(0, 6).cast(pl.UInt32),
+        )
+        dfs.append(df)
+    return pl.concat(dfs)
+
+
 if __name__ == "__main__":
     codes = ["510050.OF", "159901.OF"]
     target_dt = dt.date.today()
